42f680e12d957b4b7503bae6af9c674ec582c9c0
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-11-13 20:45:36 +0530

    creating main window and packing in the text

---
+++ first.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+#Creating a lable widget
+mylable = Label(root, text="Hello World!")
+
+#Displaying it on the window or screen
+mylable.pack()
+
+
+root.mainloop()
